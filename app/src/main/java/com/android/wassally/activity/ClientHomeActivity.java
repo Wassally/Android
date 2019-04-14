b1ace300941aea0f8fe22540c79cc5fbdc99fa32
Essam <esammosbah1@gmail.com>
2019-04-14 02:52:59 +0200

    implement navigation drawer

--- app/src/main/java/com/android/wassally/activity/ClientHomeActivity.java
+++ app/src/main/java/com/android/wassally/activity/ClientHomeActivity.java
@@ -1,22 +1,73 @@
 package com.android.wassally.activity;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Switch;
 
 import com.android.wassally.R;
+import com.android.wassally.fragment.FavoriteFragment;
+import com.android.wassally.fragment.HistoryFragment;
 
-public class ClientHomeActivity extends AppCompatActivity {
-    private TextView mClientNameTextView;
+public class ClientHomeActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_home);
-
-        mClientNameTextView = findViewById(R.id.tv_clientName);
-
         String clientName = getIntent().getStringExtra("full_name");
-        mClientNameTextView.setText(clientName);
+
+        //use our toolBar as the action bar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        //Handle menu icon at the top left to open and close the drawer with simple animation
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        //if drawer is open when pressing back button just close the navigation drawer
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_history:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HistoryFragment()).commit();
+                break;
+
+            case R.id.nav_favorite:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FavoriteFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
